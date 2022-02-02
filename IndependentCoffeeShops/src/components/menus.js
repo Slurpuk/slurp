02e0e45b346dbb2c0c183355e36203073e76df3c
SeanStanfield <seanstanfield0@gmail.com>
2022-02-02 20:12:07 +0000

    merged stylesheet with menuList

--- IndependentCoffeeShops/src/components/menus.js
+++ IndependentCoffeeShops/src/components/menus.js
@@ -1,15 +1,22 @@
-import React, { Component } from "react";
-import { View, FlatList, StyleSheet, Text, Dimensions, Animated, TouchableOpacity } from "react-native";
-import MenuList from "./MenuList";
+import React, {Component} from 'react';
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  Dimensions,
+  Animated,
+  TouchableOpacity,
+} from 'react-native';
+import MenuList from './MenuList';
 
 export default function PrimaryButton(props) {
   const {text} = props;
-  return <View>
-      <MenuList/>
-      <MenuList/>
-      <MenuList/>
+  return (
+    <View>
+      <MenuList />
+      <MenuList />
+      <MenuList />
     </View>
   );
 }
-
-
