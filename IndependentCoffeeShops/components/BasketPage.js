fcc54e9daaec17a7e23b11cd86b771d511258597
sysunia <sonia.koszut@kcl.ac.uk>
2022-02-05 21:50:37 +0000

    add basket page component

---
+++ IndependentCoffeeShops/components/BasketPage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import BasketHeader from './BasketHeader';
+import BasketContents from './BasketContents';
+
+const BasketPage = () => {
+  return (
+    <View style={styles.basket}>
+      <BasketHeader style={styles.header} coffeShopName={'ETEN & DRIKEN'} />
+      <View style={styles.main_container}>
+        <BasketContents style={styles.contents} />
+        <View style={styles.buttons}>
+          <Text style={styles.button}>FIRST BEAUTIFUL BUTTON HERE</Text>
+          <Text style={styles.button}>SECOND BEAUTIFUL BUTTON HERE</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  main_container: {
+    backgroundColor: '#E5E5E5',
+    display: 'flex',
+    height: '100%',
+    padding: '5%',
+  },
+  basket: {
+    display: 'flex',
+  },
+  header: {
+    flex: 1,
+  },
+  content: {
+    flex: 3,
+  },
+  buttons: {
+    flex: 1,
+    display: 'flex',
+    alignSelf: 'center',
+  },
+});
+
+export default BasketPage;
