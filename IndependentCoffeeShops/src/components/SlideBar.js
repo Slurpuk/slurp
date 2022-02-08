0bbe54c86cf62b5f965b52c8a8958aa49925d8b2
sysunia <sonia.koszut@kcl.ac.uk>
2022-02-08 17:52:31 +0000

    apply to new changes

--- IndependentCoffeeShops/src/components/SlideBar.js
+++ IndependentCoffeeShops/src/components/SlideBar.js
@@ -85,7 +85,6 @@ function CustomDrawerContent(props) {
 function MyDrawer() {
   return (
     <Drawer.Navigator
-        style={styles.navigator}
       drawerContent={props => <CustomDrawerContent {...props} />}
       screenOptions={({navigation}) => ({
         drawerPosition: 'right',
@@ -148,9 +147,6 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontFamily: 'Poppins',
   },
-    navigator: {
-      borderRadius: 20,
-    }
 });
 
 export default function SlideBar() {
