d6e66523bb6630ba662c8370882cdd608573cc93
Jad Sbai <jad.sbai@kcl.ac.uk>
2022-02-08 17:41:11 +0000

    Partly fix the welcome page styles

--- IndependentCoffeeShops/components/WelcomeSlider.js
+++ IndependentCoffeeShops/components/WelcomeSlider.js
@@ -13,6 +13,7 @@ import {
 import Cafe from '../src/assets/svgs/cafe.svg';
 import CoffeeBean from '../src/assets/svgs/coffee-bean.svg';
 import CoffeeCup from '../src/assets/svgs/coffee-cup.svg';
+import { green } from "react-native-reanimated/src/reanimated2/Colors";
 
 const App = () => {
     const [sliderState, setSliderState] = useState({ currentPage: 0 });
@@ -50,11 +51,9 @@ const App = () => {
     const { currentPage: pageIndex } = sliderState;
 
     return (
-        <>
-
-            <SafeAreaView style={{ flex: 1 }}>
+            <SafeAreaView style={{display: "flex", height:'100%'}}>
                 <ScrollView
-                    style={{ flex: 1 }}
+                    style={{display: "flex", flex: 1 }}
                     horizontal={true}
                     scrollEventThrottle={16}
                     pagingEnabled={true}
@@ -63,72 +62,81 @@ const App = () => {
                         setSliderPage(event);
                     }}
                 >
-                    <View style={{ width, height }}>
+                    <View style={{ width, height, }}>
                         <View style={styles.container}>
                             <Text style={styles.title}>Welcome to the app!</Text>
                             <CoffeeCup stroke={'#333'} height={200} width={200}  />
                             <Text style={styles.text}>Why our service is awesome part 1</Text>
                         </View>
                     </View>
-                    <View style={{ width, height }}>
+                    <View style={{ width, height}}>
                         <View style={styles.container}>
                             <Text style={styles.title}>Welcome Part 2!</Text>
                             <CoffeeBean stroke={'#333'} height={200} width={200} />
                             <Text style={styles.text}>Why our service is awesome part 2</Text>
                         </View>
                     </View>
-                    <View style={{ width, height }}>
-                        <View style={styles.container}>
+                        <View style={[{ width, height, backgroundColor: 'green'}, styles.ViewWrapper]}>
                             <Text style={styles.title}>Welcome Part 3!</Text>
-                            <Cafe stroke={'#333'} height={200} width={200} />
+                            <Cafe style = {styles.svgImg} stroke={'#333'} height={200} width={200} />
                             <Text style={styles.text}>Why you should create an account </Text>
                             <Pressable
                                 style={[styles.button, styles.account_button]}
                                 onPress={createAccount}>
-                                <Text style={[styles.text, styles.button_text]}>Create an Account</Text>
+                                <Text style={styles.button_text}>Create an Account</Text>
                             </Pressable>
                         </View>
-                    </View>
                 </ScrollView>
                 <View style={styles.paginationWrapper}>
                     {Array.from(Array(3).keys()).map((key, index) => (
-                        <View style={[styles.paginationDots, { opacity: pageIndex === index ? 1 : 0.2 }]} key={index} />
+                        <View style={[styles.paginationDots, { opacity: pageIndex === index ? 1 : 0.7 }]} key={index} />
                     ))}
                 </View>
             </SafeAreaView>
-        </>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 150,
-        flex: 1,
         padding: 5,
+    },
+
+    ViewWrapper:{
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        position: "relative"
+
+    },
+
+    svgImg:{
+        marginTop: '15%'
     },
     text: {
         fontFamily: 'Josefin Sans',
         fontSize: 20,
         color: '#173C4F',
-        flex: 1,
+        marginTop: '15%',
+        marginBottom: '8%',
+        flex: 0
+
     },
     title: {
         fontFamily: 'Josefin Sans',
         fontWeight:'bold',
         color: '#173C4F',
         fontSize: 30,
-        flex: 1,
+        backgroundColor: 'orange',
+        marginTop: '15%'
+
     },
     paginationWrapper: {
-        position: 'absolute',
-        bottom: 70,
-        left: 0,
-        right: 0,
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        minHeight: 40,
+        backgroundColor: 'purple'
     },
     paginationDots: {
         height: 10,
@@ -136,21 +144,20 @@ const styles = StyleSheet.create({
         borderRadius: 10 / 2,
         backgroundColor: '#C38C22',
         marginLeft: 10,
+        flex: 0
     },
     button: {
         borderRadius: 13,
-        height: 41,
-    },
-    account_button: {
         backgroundColor: '#087562',
+
     },
+
     button_text: {
         fontFamily: 'Josefin Sans',
         fontWeight:'bold',
         color: '#EFEFEF',
         textAlign: 'center',
         textAlignVertical: 'center',
-        height: '100%',
     }
 });
 export default App;
