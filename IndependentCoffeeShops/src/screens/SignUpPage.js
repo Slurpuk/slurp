a0d04682564568b68bbee5c9124cc1bb49d0c6ea
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-02-11 18:16:59 +0000

    Update sign up page with new refactored components

--- IndependentCoffeeShops/src/screens/SignUpPage.js
+++ IndependentCoffeeShops/src/screens/SignUpPage.js
@@ -7,8 +7,16 @@
  */
 
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, Alert} from 'react-native';
-import SignUpForm from '../components/UserManagement/SignUpForm';
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
+import textStyles from '../../stylesheets/textStyles';
+import PrimaryButton from '../SubComponents/PrimaryButton';
+import FormField from '../components/UserManagement/FormField';
 
 // Redirect the user to the Log In Portal
 const switchToLogIn = () => {
@@ -23,23 +31,109 @@ const switchToLogIn = () => {
   );
 };
 
+// Display a confirmation message to the user
+const registeredMessage = () => {
+  Alert.alert('Congratulations', 'Registered Successfully', [
+    {
+      text: 'OK',
+    },
+  ]);
+};
+
+// Register the user to the database after checking their credentials
+const registerUser = () => {
+  //     auth().createUserWithEmailAndPassword(email,password)
+  //         .then((re)=>{
+  //             console.log(re);
+  //             console.log(x);
+  //         })
+  //         .catch((re)=>{
+  //             console.log(re);
+  //         })
+  registeredMessage();
+};
+
 const SignUpPage = () => {
+  // const usersCollection = firestore().collection('Users');
+  const [first_name, setFirstName] = useState();
+  const [last_name, setLastName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [password_confirmation, setPasswordConfirmation] = useState();
+
   return (
-    <View style={styles.body}>
-      <Text style={[styles.title, styles.text]}>Sign Up</Text>
-      <SignUpForm style={styles.form}></SignUpForm>
-      <Text style={(styles.text, styles.footer)} onPress={switchToLogIn}>
-        Already have an account? Log in here
-      </Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.body}>
+        <View style={styles.body}>
+          <Text style={[textStyles.headingOne, styles.section]}>Sign Up</Text>
+
+          <View style={styles.form}>
+            <View style={styles.name_container}>
+              <FormField
+                style={styles.sub_name_container}
+                title={'First Name'}
+                placeholder={'Joe'}
+                setField={setFirstName}
+                isPassword={false}
+              />
+              <FormField
+                style={styles.sub_name_container}
+                title={'Last Name'}
+                placeholder={'Doe'}
+                setField={setLastName}
+                isPassword={false}
+              />
+            </View>
+            <FormField
+              style={styles.element}
+              title={'Email'}
+              placeholder={'johnsmith@gmail.com'}
+              setField={setEmail}
+              isPassword={false}
+            />
+            <FormField
+              style={styles.element}
+              title={'Password'}
+              placeholder={''}
+              setField={setPassword}
+              isPassword={true}
+            />
+            <FormField
+              style={styles.element}
+              title={'Confirm Password'}
+              placeholder={''}
+              setField={setPasswordConfirmation}
+              isPassword={true}
+            />
+          </View>
+
+          <View style={styles.buttons_container}>
+            <View style={styles.button_container}>
+              <PrimaryButton text={'Set Coffee Preferences'} onPress={null} />
+            </View>
+            <View style={styles.button_container}>
+              <PrimaryButton text={'Create Account'} onPress={registerUser} />
+            </View>
+
+            <Text style={(styles.text, styles.footer)} onPress={switchToLogIn}>
+              Already have an account? Log in here
+            </Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  safe_header: {
+    flex: 0,
+    backgroundColor: '#EDEBE7',
+  },
   body: {
     backgroundColor: '#EDEBE7',
-    padding: '5%',
     flex: 1,
+    padding: '5%',
   },
   title: {
     fontSize: 35,
@@ -54,12 +148,41 @@ const styles = StyleSheet.create({
   },
   form: {
     flex: 3,
+    paddingVertical: '5%',
   },
   footer: {
     textAlign: 'center',
     textDecorationLine: 'underline',
     flex: 0.2,
     textAlignVertical: 'bottom',
+  },
+
+  buttons_container: {
+    alignContent: 'flex-end',
+  },
+  button_container: {
+    paddingVertical: '2%',
+  },
+
+  name_container: {
+    flexDirection: 'row',
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingVertical: '1%',
+  },
+  sub_name_container: {
+    flex: 1,
+  },
+  element: {
+    fontSize: 16,
+    paddingVertical: '1%',
+  },
+
+  input: {
+    backgroundColor: '#F9F9F9',
+    width: '100%',
+    height: 37,
+    borderRadius: 5,
   },
 });
 
