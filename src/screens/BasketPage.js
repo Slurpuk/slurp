c90985ce292547ad4b3451ef02f859a116640702
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-02-13 16:04:24 +0000

    Refactor to utilise versatile header
    -Restyle for better use of safe area view on android and ios
    -Centre text
    -Create custom arrow button. Maintainable and versatile.

--- src/screens/BasketPage.js
+++ src/screens/BasketPage.js
@@ -1,28 +1,40 @@
 import React from 'react';
-import {StyleSheet, Text, View, Fragment, SafeAreaView} from 'react-native';
-import BasketHeader from '../components/Basket/BasketHeader';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Fragment,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import GreenHeader from '../components/GreenHeader';
 import BasketContents from '../components/Basket/BasketContents';
-import PrimaryButton from '../SubComponents/PrimaryButton';
+import CustomButton from '../SubComponents/CustomButton';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
 
 const BasketPage = () => {
   return (
-    <>
-      <SafeAreaView style={styles.safe_header}></SafeAreaView>
-      <SafeAreaView style={styles.basket}>
-        <View style={styles.basket}>
-          <View style={styles.header}>
-            <BasketHeader coffeShopName={'ETEN & DRIKEN'} />
-          </View>
-          <View style={styles.main_container}>
-            <BasketContents/>
-          </View>
-          <View style={styles.buttons}>
-            <PrimaryButton style={styles.button} text={'Apple/Google pay'} />
-            <PrimaryButton style={styles.button} text={'Checkout with card'} />
-          </View>
-        </View>
-      </SafeAreaView>
-    </>
+    <View style={styles.basket}>
+      <View style={styles.header}>
+        <GreenHeader headerText={'ETEN & DRIKEN'} />
+      </View>
+      <View style={styles.main_container}>
+        <BasketContents />
+      </View>
+      <View style={styles.buttons}>
+        <CustomButton
+          priority="primary"
+          style={styles.button}
+          text={'Apple/Google pay'}
+        />
+        <CustomButton
+          priority="primary"
+          style={styles.button}
+          text={'Checkout with card'}
+        />
+      </View>
+    </View>
   );
 };
 
@@ -35,11 +47,10 @@ const styles = StyleSheet.create({
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
-
     backgroundColor: '#E5E5E5',
   },
   header: {
-    flexShrink: 2,
+    flexShrink: 1,
   },
   main_container: {
     flexShrink: 10,
@@ -48,12 +59,12 @@ const styles = StyleSheet.create({
   },
   buttons: {
     flexShrink: 4,
-    marginVertical: '4%',
+    marginVertical: '8%',
+    alignItems: 'center',
     justifyContent: 'flex-end',
   },
   button: {
-    marginVertical: '2%',
-
+    paddingVertical: '5%',
   },
 });
 
--- src/screens/SignUpPage.js
+++ src/screens/SignUpPage.js
@@ -7,19 +7,12 @@
  */
 
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  SafeAreaView,
-  KeyboardAvoidingView,
-  Text,
-  Alert,
-  Linking,
-} from 'react-native';
+import {StyleSheet, View, Text, Alert, StatusBar, Platform} from 'react-native';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import textStyles from '../../stylesheets/textStyles';
 import PrimaryButton from '../SubComponents/PrimaryButton';
 import FormField from '../components/UserManagement/FormField';
-import linkingContext from '@react-navigation/native/src/LinkingContext';
 
 // Redirect the user to the Log In Portal
 const switchToLogIn = () => {
@@ -65,76 +58,86 @@ const SignUpPage = () => {
   const [password_confirmation, setPasswordConfirmation] = useState();
 
   return (
-    <SafeAreaView style={styles.safeSpace}>
-      <View style={styles.body}>
-        <Text style={[textStyles.blueJosefinHeading]}>Sign Up</Text>
+    <View style={styles.wrapper}>
+      <StatusBar translucent={true} backgroundColor="transparent" />
+      <View style={styles.safeSpace}>
+        <View style={styles.body}>
+          <Text style={[textStyles.blueJosefinHeading]}>Sign Up</Text>
 
-        <View style={styles.form}>
-          <View style={styles.name_container}>
+          <View style={styles.form}>
+            <View style={styles.name_container}>
+              <FormField
+                style={[
+                  styles.sub_name_container,
+                  styles.sub_name_container_left,
+                ]}
+                title={'First Name'}
+                placeholder={'John'}
+                setField={setFirstName}
+                type={'name'}
+              />
+              <FormField
+                style={[styles.sub_name_container]}
+                title={'Last Name'}
+                placeholder={'Doe'}
+                setField={setLastName}
+                type={'name'}
+              />
+            </View>
             <FormField
-              style={[
-                styles.sub_name_container,
-                styles.sub_name_container_left,
-              ]}
-              title={'First Name'}
-              placeholder={'John'}
-              setField={setFirstName}
-              type={'name'}
+              style={styles.element}
+              title={'Email'}
+              placeholder={'johndoe@gmail.com'}
+              setField={setEmail}
+              type={'email'}
             />
             <FormField
-              style={[styles.sub_name_container]}
-              title={'Last Name'}
-              placeholder={'Doe'}
-              setField={setLastName}
-              type={'name'}
+              style={styles.element}
+              title={'Password'}
+              setField={setPassword}
+              type={'password'}
+            />
+            <FormField
+              style={styles.element}
+              title={'Confirm Password'}
+              setField={setPasswordConfirmation}
+              type={'password'}
             />
           </View>
-          <FormField
-            style={styles.element}
-            title={'Email'}
-            placeholder={'johndoe@gmail.com'}
-            setField={setEmail}
-            type={'email'}
-          />
-          <FormField
-            style={styles.element}
-            title={'Password'}
-            setField={setPassword}
-            type={'password'}
-          />
-          <FormField
-            style={styles.element}
-            title={'Confirm Password'}
-            setField={setPasswordConfirmation}
-            type={'password'}
-          />
-        </View>
 
-        <View style={styles.buttons_container}>
-          <View style={styles.button_container}>
-            <PrimaryButton text={'Set Coffee Preferences'} onPress={null} />
-          </View>
-          <View style={styles.button_container}>
-            <PrimaryButton text={'Create Account'} onPress={registerUser} />
-          </View>
-          <View>
-            <Text
-              style={[textStyles.bluePoppinsBody, styles.footer]}
-              onPress={switchToLogIn}
-            >
-              Already have an account? Log in here
-            </Text>
+          <View style={styles.buttons_container}>
+            <View style={styles.button_container}>
+              <PrimaryButton text={'Set Coffee Preferences'} onPress={null} />
+            </View>
+            <View style={styles.button_container}>
+              <PrimaryButton text={'Create Account'} onPress={registerUser} />
+            </View>
+            <View>
+              <Text
+                style={[textStyles.bluePoppinsBody, styles.footer]}
+                onPress={switchToLogIn}
+              >
+                Already have an account? Log in here
+              </Text>
+            </View>
           </View>
         </View>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
   safeSpace: {
     flex: 1,
     backgroundColor: '#EDEBE7',
+    paddingTop:
+      Platform.OS === 'android'
+        ? getStatusBarHeight() / 3
+        : getStatusBarHeight(),
   },
   body: {
     flex: 1,
