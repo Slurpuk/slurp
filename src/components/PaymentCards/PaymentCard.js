ac1692c81b0e42646df772caac65b6c8392c4e34
sysunia <sonia.koszut@kcl.ac.uk>
2022-02-14 06:11:07 +0000

    prepare a set up

--- src/components/PaymentCards/PaymentCard.js
+++ src/components/PaymentCards/PaymentCard.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View, Pressable, Image, Text, Alert, SafeAreaView} from 'react-native';
-import Header from '../SubComponents/Header'
-import PrimaryButton from '../SubComponents/PrimaryButton'
 
-const PaymentCard = () => {
+const PaymentCard = ({card}) => {
+
+    console.log(card)
 
     return (
-        <View>
-            <Text>Card</Text>
+        <View style={styles.rectangle}>
+            <View>
+                <Image source={'..'}/>
+                <Text>Visa Debit</Text>
+            </View>
+            <Text>{card.number}</Text>
+            {card.isDefault ? <Text>Default</Text> : <Text>Secondary</Text>}
+            <View>
+                <Text>Delete</Text>
+            </View>
+            <View>
+                <Text>Set as Default</Text>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-
+    rectangle: {
+        backgroundColor: '#F2F2F2',
+        display: 'flex'
+    }
 });
 
 export default PaymentCard;
--- src/screens/PaymentCardsPage.js
+++ src/screens/PaymentCardsPage.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import {StyleSheet, View, Pressable, Image, Text, Alert, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Pressable, Image, Text, Alert, SafeAreaView, FlatList} from 'react-native';
 import Header from '../SubComponents/Header'
 import PrimaryButton from '../SubComponents/PrimaryButton'
+import PaymentCardsData from "../fake-data/PaymentCardsData";
+import PaymentCard from "../components/PaymentCards/PaymentCard";
 
 const PaymentCardsPage = () => {
 
+    const [cards, setCards] = useState(PaymentCardsData)
+
     return (
         <SafeAreaView>
-            <Header title={'PAYMENT CARDS'}></Header>
-            <View><Text>Active Cards Here</Text></View>
-            <PrimaryButton text={'Add New Payment Card'}></PrimaryButton>
+            <Header title={'PAYMENT CARDS'}/>
+            <FlatList data={cards} renderItem={({item}) =>
+                <PaymentCard card={item}/>
+            }/>
+            <PrimaryButton text={'Add New Payment Card'}/>
         </SafeAreaView>
     );
 };
