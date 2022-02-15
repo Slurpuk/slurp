f4f058e95b97ee7dedc4792bac9ba76fdb15c3e4
sysunia <sonia.koszut@kcl.ac.uk>
2022-02-15 00:03:30 +0000

    further improve

--- src/components/PaymentCards/PaymentCard.js
+++ src/components/PaymentCards/PaymentCard.js
@@ -1,24 +1,40 @@
 import React, {useState} from 'react';
 import {StyleSheet, View, Pressable, Image, Text, Alert, SafeAreaView} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const PaymentCard = ({card}) => {
 
-    console.log(card)
+    function deleteCard(){
+        console.log('delete')
+    }
+
+    function setAsDefault(){
+        console.log('set as default')
+    }
 
     return (
         <View style={styles.rectangle}>
-            <View>
-                <Image source={'..'}/>
-                <Text>Visa Debit</Text>
+            <View style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'row'}}>
+                <View style={{display: 'flex', flexDirection: 'row'}}>
+                    {/*<Image source={require('./visa.svg')} height={24}/>*/}
+                    <Text style={[styles.text, {color: 'black', fontWeight: '600', fontSize: 14}]}>{card.type}</Text>
+                </View>
+                <Pressable onPress={deleteCard} style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    <Icon name={'trash-can'} size={24} color={'#CD5160'}/>
+                    <Text style={[styles.text, {color:'#CD5160'}]}>Delete</Text>
+                </Pressable>
             </View>
-            <Text>{card.number}</Text>
-            {card.isDefault ? <Text>Default</Text> : <Text>Secondary</Text>}
-            <View>
-                <Text>Delete</Text>
+            <Text>*** **** **** {card.number.slice(13,19)}</Text>
+            {card.isDefault ?
+            <View style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'row'}}>
+                <Text style={[{color: '#727272', fontWeight: '300', fontSize: 11}, styles.text]}>
+                    Default
+                </Text>
+                <Pressable onPress={setAsDefault}>
+                    <Text style={[styles.text, {color: '#1B947E'}]}>Set as Default</Text>
+                </Pressable>
             </View>
-            <View>
-                <Text>Set as Default</Text>
-            </View>
+            : <Text style={[{color: '#727272', fontWeight: '300', fontSize: 11}, styles.text]}>Secondary</Text>}
         </View>
     );
 };
@@ -26,7 +42,23 @@ const PaymentCard = ({card}) => {
 const styles = StyleSheet.create({
     rectangle: {
         backgroundColor: '#F2F2F2',
-        display: 'flex'
+        display: 'flex',
+        padding: '3.5%',
+        borderRadius: 10,
+        marginBottom: '5%',
+        elevation: 1,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.18,
+        shadowRadius: 1.00,
+    },
+    text: {
+        fontFamily: 'Poppins',
+        fontSize: 12,
+        fontWeight: '400',
     }
 });
 
--- src/screens/PaymentCardsPage.js
+++ src/screens/PaymentCardsPage.js
@@ -1,27 +1,39 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View, Pressable, Image, Text, Alert, SafeAreaView, FlatList} from 'react-native';
 import Header from '../SubComponents/Header'
 import PrimaryButton from '../SubComponents/PrimaryButton'
 import PaymentCardsData from "../fake-data/PaymentCardsData";
 import PaymentCard from "../components/PaymentCards/PaymentCard";
 
+export const PaymentCardsContext = React.createContext();
+
 const PaymentCardsPage = () => {
 
     const [cards, setCards] = useState(PaymentCardsData)
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.page}>
             <Header title={'PAYMENT CARDS'}/>
-            <FlatList data={cards} renderItem={({item}) =>
-                <PaymentCard card={item}/>
-            }/>
+            <PaymentCardsContext.Provider
+                value={{
+                    cards: cards,
+                    setCards: setCards,
+                }}>
+                <FlatList data={cards} renderItem={({item}) =>
+                    <PaymentCard card={item}/>
+                } style={{padding: '5%'}}/>
+            </PaymentCardsContext.Provider>
             <PrimaryButton text={'Add New Payment Card'}/>
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-
+    page:{
+        display: 'flex',
+        justifyContent: 'space-between',
+        backgroundColor: '#E5E5E5',
+    }
 });
 
 export default PaymentCardsPage;
