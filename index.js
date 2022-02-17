0bed54b3c7d32195258871ab51c1e2ac0428681f
Jad Sbai <jad.sbai@kcl.ac.uk>
2022-02-17 09:37:43 +0000

    Set up Firebase for the User app:
    - Link to firebase
    - Set up authentication
    - Link to firestore
    - Set up cloud functions deployment
    - Set up the firebase project in the firebase folder

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './App';
 import testers from "./src/testers";
+import RegisterEmailPassword from "./src/firebase/authentication/registerEmailPassword";
+import Query from "./src/firebase/firestore/basicQuery";
 
-AppRegistry.registerComponent(appName, () =>testers.NavigableLandingPage);
+AppRegistry.registerComponent(appName, () => testers.NavigableLandingPage);
 
