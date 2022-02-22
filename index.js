552d50696497ab585f30308bea3461133e816116
meyad-g <meyad.golmakani@kcl.ac.uk>
2022-02-22 13:54:29 +0000

    resolve conflict

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './App';
-import AnimatedCard from "./src/sub-components/AnimatedCard";
+import SignUpPage from "./src/screens/SignUpPage";
+import LogInPage from "./src/screens/LogInPage";
+import testers from "./src/testers";
+import BasketPage from "./src/screens/BasketPage";
+import UpdateDetailsPage from "./src/screens/UpdateDetailsPage";
+import ChangePasswordPage from "./src/screens/ChangePasswordPage";
+import PaymentCardsPage from "./src/screens/PaymentCardsPage";
+import AddNewCardPage from "./src/screens/AddNewCardPage";
 
-AppRegistry.registerComponent(appName, () => AnimatedCard);
+
+AppRegistry.registerComponent(appName, () => PaymentCardsPage);
