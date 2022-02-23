a55b9790f3b9dd7fb1089307184e3d9880bf1458
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-02-23 16:19:55 +0000

    Create a json with fake data for FE testing

---
+++ src/fake-data/OrderData.js
@@ -0,0 +1,141 @@
+const orders = [
+  {
+    period: 'December 2021',
+    key: 'December 2021',
+    data: [
+      {
+        key: 54,
+        orderNumber: 654321,
+        status: 'Cancelled',
+        coffeeShopName: 'Eten & Driken',
+        date: '30th Nov',
+        time: '18:20',
+        items: [
+          {
+            name: 'Latte',
+            quantity: 2,
+            price: 2.3,
+            milk: 'Dairy milk',
+            syrup: null,
+          },
+        ],
+      },
+      {
+        key: 55,
+        orderNumber: 135790,
+        status: 'Collected',
+        coffeeShopName: 'Expresso Room',
+        date: '30th Nov',
+        time: '12:00',
+        items: [
+          {
+            name: 'Latte',
+            quantity: 2,
+            price: 2.3,
+            milk: 'Dairy milk',
+            syrup: null,
+          },
+          {
+            name: 'Cappuchino',
+            quantity: 1,
+            price: 3.1,
+            milk: 'Oat Milk',
+            syrup: 'Caramel',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    period: 'November 2021',
+    key: 'November 2021',
+    data: [
+      {
+        key: 56,
+        orderNumber: 123456,
+        status: 'Collected',
+        coffeeShopName: 'Eten & Driken',
+        date: '30th Nov',
+        time: '18:20',
+        items: [
+          {
+            name: 'Latte',
+            quantity: 2,
+            price: 2.3,
+            milk: 'Dairy milk',
+            syrup: null,
+          },
+        ],
+      },
+      {
+        key: 56,
+        orderNumber: 123456,
+        coffeeShopName: 'Eten & Driken',
+        date: '30th Nov',
+        time: '18:20',
+        items: [
+          {
+            name: 'Latte',
+            quantity: 2,
+            price: 2.3,
+            milk: 'Dairy milk',
+            syrup: null,
+          },
+        ],
+      },
+      {
+        key: 56,
+        orderNumber: 123456,
+        status: 'Collected',
+        coffeeShopName: 'Eten & Driken',
+        date: '30th Nov',
+        time: '18:20',
+        items: [
+          {
+            name: 'Latte',
+            quantity: 2,
+            price: 2.3,
+            milk: 'Dairy milk',
+            syrup: null,
+          },
+        ],
+      },
+      {
+        key: 56,
+        orderNumber: 123456,
+        status: 'Collected',
+        coffeeShopName: 'Eten & Driken',
+        date: '30th Nov',
+        time: '18:20',
+        items: [
+          {
+            name: 'Latte',
+            quantity: 2,
+            price: 2.3,
+            milk: 'Dairy milk',
+            syrup: null,
+          },
+        ],
+      },
+      {
+        key: 56,
+        orderNumber: 123456,
+        status: 'Cancelled',
+        coffeeShopName: 'Eten & Driken',
+        date: '30th Nov',
+        time: '18:20',
+        items: [
+          {
+            name: 'Latte',
+            quantity: 2,
+            price: 2.3,
+            milk: 'Dairy milk',
+            syrup: null,
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default orders;
