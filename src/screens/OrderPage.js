bd66d2a670eec64bb813982ee05ebc9e6afe9d0c
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-02-26 02:22:22 +0000

    Segregate past and current orders in order page (separate components)

--- src/screens/OrderPage.js
+++ src/screens/OrderPage.js
@@ -1,31 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View, SectionList, StatusBar, ScrollView } from "react-native";
+import {StyleSheet, Text, View, SectionList, FlatList} from 'react-native';
 import CollapsedOrder from '../components/Orders/CollapsableOrder';
 import textStyles from '../../stylesheets/textStyles';
 import GreenHeader from '../sub-components/GreenHeader';
-import orders from '../fake-data/OrderData';
+import pastOrders from '../fake-data/PastOrderData';
+import currentOrders from '../fake-data/CurrentOrderData';
 
 const OrderPage = () => {
   return (
     <View style={styles.basket}>
       <GreenHeader headerText={'ORDERS'} />
-      <SectionList
-        contentContainerStyle={styles.mainContainer}
-        sections={orders}
-        stickySectionHeadersEnabled={false}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({item}) => <CollapsedOrder order={item} />}
-        renderSectionHeader={({section: {period}}) => (
-          <Text
-            style={[textStyles.darkGreyPoppinsHeading, styles.periodHeader]}
-          >
-            {period}
-          </Text>
-        )}
-      />
+      <PastOrders />
     </View>
   );
 };
+
+const PastOrders = () => {
+  return (
+    <SectionList
+      contentContainerStyle={styles.mainContainer}
+      sections={pastOrders}
+      stickySectionHeadersEnabled={false}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({item}) => <CollapsedOrder order={item} />}
+      renderSectionHeader={({section: {period}}) => (
+        <Text style={[textStyles.darkGreyPoppinsHeading, styles.periodHeader]}>
+          {period}
+        </Text>
+      )}
+    />
+  );
+};
+
+const CurrentOrders = () => {
+  return(
+    <Text> Hi </Text>
+  );
+};
+
+// <FlatList
+//   contentContainerStyle={styles.mainContainer}
+//   sections={currentOrders}
+//   keyExtractor={item => item.orderNumber}
+//   renderItem={({item}) => <CollapsedOrder order={item} />}
+// />
 
 const styles = StyleSheet.create({
   basket: {
