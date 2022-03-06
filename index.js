725aa2ee8134e3667829d525a8c7be7cb53e20aa
Jad Sbai <jad.sbai@kcl.ac.uk>
2022-03-06 00:09:32 +0000

    Embed circle to SVG in welcome pages

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './App';
+import WelcomePages from "./src/screens/WelcomePages";
 
 AppRegistry.registerComponent(appName, () => App);
