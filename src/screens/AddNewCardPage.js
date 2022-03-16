1b026768e68f9ad81db6246b33f28917c49e7fcc
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-03-16 12:19:57 +0000

    Clean up Payment Card Page use resuable compenents and remove unused imports

--- src/screens/AddNewCardPage.js
+++ src/screens/AddNewCardPage.js
@@ -1,48 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react';
+import {StripeProvider} from '@stripe/stripe-react-native';
+import AddNewCardComponent from '../components/PaymentCards/AddNewCardComponent';
 
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, View, Text, Alert, StatusBar, Platform, Button, Dimensions, TouchableOpacity} from 'react-native';
-import { StripeProvider } from '@stripe/stripe-react-native';
-import AddNewCardComponent from "../components/PaymentCards/AddNewCardComponent";
+const AddNewCardPage = ({navigation}) => {
+  //    const [publishableKey, setPublishableKey] = useState('');
 
+  //    const fetchPublishableKey = async () => {
+  //        const key = "pk_test_51KRjSVGig6SwlicvL06FM1BDNZr1539SwuDNXond8v6Iaigyq1NRZsleWNK5PTPEwo1bAWfTQqYHEfXCJ4OWq348000jVuI6u1" // fetch key from your server here
+  //        setPublishableKey(key);
+  //    };
 
-const AddNewCardPage = () => {
+  //    useEffect(() => {
+  //        fetchPublishableKey();
+  //    }, []);
 
-//    const [publishableKey, setPublishableKey] = useState('');
+  const publishableKey =
+    'pk_test_51KRjSVGig6SwlicvL06FM1BDNZr1539SwuDNXond8v6Iaigyq1NRZsleWNK5PTPEwo1bAWfTQqYHEfXCJ4OWq348000jVuI6u1';
 
-//    const fetchPublishableKey = async () => {
-//        const key = "pk_test_51KRjSVGig6SwlicvL06FM1BDNZr1539SwuDNXond8v6Iaigyq1NRZsleWNK5PTPEwo1bAWfTQqYHEfXCJ4OWq348000jVuI6u1" // fetch key from your server here
-//        setPublishableKey(key);
-//    };
-
-//    useEffect(() => {
-//        fetchPublishableKey();
-//    }, []);
-
-    const publishableKey = "pk_test_51KRjSVGig6SwlicvL06FM1BDNZr1539SwuDNXond8v6Iaigyq1NRZsleWNK5PTPEwo1bAWfTQqYHEfXCJ4OWq348000jVuI6u1";
-
-    return (
-        <StripeProvider
-            publishableKey={publishableKey}
-            merchantIdentifier="merchant.identifier"
-        >
-            <AddNewCardComponent />
-        </StripeProvider>
-    );
+  return (
+    <StripeProvider
+      publishableKey={publishableKey}
+      merchantIdentifier="merchant.identifier"
+    >
+      <AddNewCardComponent navigation={navigation} />
+    </StripeProvider>
+  );
 };
 
-const styles = StyleSheet.create({
-    wrapper: {
-        flex: 1,
-    },
-
-});
-
 export default AddNewCardPage;
-
