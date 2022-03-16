3be1d85c681e79565556a6ad367a544b9e0e1dd4
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-03-16 12:25:38 +0000

    Remove extra imports in index

--- index.js
+++ index.js
@@ -6,13 +6,5 @@ import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import App from './App';
-import testers from "./src/testers";
-import RegisterEmailPassword from "./src/firebase/authentication/registerEmailPassword";
-import LandingMapPage from "./src/screens/LandingMapPage";
-import MapBackground from "./src/components/LandingMap/MapBackground";
-// import Query from "./src/firebase/firestore/basicQuery";
-import AddNewCardPage from "./src/screens/AddNewCardPage";
-import PaymentCardsPage from "./src/screens/PaymentCardsPage";
 
-AppRegistry.registerComponent(appName, () => PaymentCardsPage);
-// AppRegistry.registerComponent(appName, () => MapBackground);
+AppRegistry.registerComponent(appName, () => App);
