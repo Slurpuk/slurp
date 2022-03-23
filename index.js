6c5f58d611da2bfd2dabedfbe87e7520d506cbf4
Jad Sbai <jad.sbai@kcl.ac.uk>
2022-03-23 19:09:19 +0000

    Working version of stripe payment merged with master

--- index.js
+++ index.js
@@ -4,13 +4,7 @@
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import App from './App';
-import WelcomePages from "./src/screens/WelcomePages";
-import LandingMapPage from "./src/screens/LandingMapPage";
-import testers from "./src/testers";
-import MapBackground from "./src/components/LandingMap/MapBackground";
 import PaymentPage from "./src/screens/PaymentPage";
-import PaymentScreen from "./src/components/PaymentCards/PaymentScreen";
-import OrderPage from "./src/screens/OrderPage";
+
 
 AppRegistry.registerComponent(appName, () => PaymentPage);
