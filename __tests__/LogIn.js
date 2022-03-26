f3d522a6213ef46fc000d16da545428953cea8a4
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-03-26 05:17:53 +0000

    Add tests for all log in front end functionality

--- __tests__/LogIn.js
+++ __tests__/LogIn.js
@@ -1,38 +1,138 @@
-import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 import React from 'react';
-import App, {GlobalContext} from '../App';
 import LogInPage from '../src/screens/LogInPage';
 import {Alert} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
 
 describe('Login page', function () {
+  const spyAlert = jest
+    .spyOn(Alert, 'alert')
+    //@ts-ignore
+    .mockImplementation((title, message, callbackOrButtons) =>
+      callbackOrButtons[1].onPress(),
+    );
+
   it('should render correctly', function () {
-    const {getByTestId, getByText, queryByTestId, toJSON} = render(
-      <LogInPage />,
-    );
-    expect(toJSON()).toMatchSnapshot();
+    expect(render(<LogInPage />).toJSON()).toMatchSnapshot();
   });
-  it("clicking on the 'new?' button takes you to sign up page", async function () {
-    const component = <App />;
 
-    const {queryByText, findByText} = render(component);
-    const oldScreen = queryByText('Log in');
-    const button = await findByText('New? Create an account');
+  describe('log in', function () {
+    it('should raise alert on submit with empty email', async function () {
+      const {getByText} = render(<LogInPage />);
 
-    expect(oldScreen).toBeTruthy();
+      fireEvent(getByText('Log in'), 'press');
 
-    fireEvent(button, 'press');
-    const newScreen = await findByText('Sign up');
+      expect(spyAlert).toHaveBeenCalled();
+      expect(spyAlert.mock.calls[0][0]).toBe('Empty Email');
+    });
+    it('should raise alert on email without @', async function () {
+      spyAlert.mockClear();
 
-    expect(newScreen).toBeTruthy();
+      const {getByText, getAllByPlaceholderText} = render(<LogInPage />);
+
+      const inputs = getAllByPlaceholderText('');
+      expect(inputs[0]).toBeTruthy();
+
+      fireEvent.changeText(inputs[0], 'emailwithoutaroba.com');
+
+      fireEvent(getByText('Log in'), 'press');
+
+      expect(spyAlert).toHaveBeenCalled();
+      expect(spyAlert.mock.calls[0][0]).toBe('Bad Email');
+    });
+    it('should raise alert on email with empty domain name', async function () {
+      spyAlert.mockClear();
+
+      const {getByText, getAllByPlaceholderText} = render(<LogInPage />);
+
+      const inputs = getAllByPlaceholderText('');
+      expect(inputs[0]).toBeTruthy();
+
+      fireEvent.changeText(inputs[0], 'email@');
+
+      fireEvent(getByText('Log in'), 'press');
+
+      expect(spyAlert).toHaveBeenCalled();
+      expect(spyAlert.mock.calls[0][0]).toBe('Bad Email');
+    });
+    it('should raise alert on empty password', async function () {
+      spyAlert.mockClear();
+
+      const {getByText, getAllByPlaceholderText} = render(<LogInPage />);
+
+      const inputs = getAllByPlaceholderText('');
+      expect(inputs[0]).toBeTruthy();
+
+      fireEvent.changeText(inputs[0], 'janedoe@gmail.com');
+
+      expect(inputs[1]).toBeTruthy();
+
+      fireEvent(getByText('Log in'), 'press');
+
+      expect(spyAlert).toHaveBeenCalled();
+      expect(spyAlert.mock.calls[0][0]).toBe('Empty Password');
+    });
+    it('should not raise alert on valid data', async function () {
+      spyAlert.mockClear();
+
+      const {getByText, getAllByPlaceholderText} = render(<LogInPage />);
+
+      const inputs = getAllByPlaceholderText('');
+      expect(inputs[0]).toBeTruthy();
+
+      fireEvent.changeText(inputs[0], 'janedoe@gmail.com');
+      fireEvent.changeText(inputs[1], 'Password123');
+
+      expect(inputs[1]).toBeTruthy();
+
+      fireEvent(getByText('Log in'), 'press');
+
+      expect(spyAlert).toHaveBeenCalledTimes(0);
+    });
   });
-  it('should trigger password reset on forgot password link', async function () {
-    const logInPage = render(<LogInPage />);
-    const toClick = await logInPage.findByText('Forgot your password?');
-    fireEvent(toClick, 'press');
-    jest.spyOn(Alert, 'alert');
-    expect(Alert.alert).toHaveBeenCalledWith(
-      'Please enter your email correctly in the field above to reset your password.',
-    );
+  describe('forgot password', function () {
+    it('should raise alert on empty email when clicking forgot password', async function () {
+      spyAlert.mockClear();
+
+      const {getByText, getAllByPlaceholderText} = render(<LogInPage />);
+
+      const inputs = getAllByPlaceholderText('');
+      expect(inputs[0]).toBeTruthy();
+
+      fireEvent.changeText(inputs[0], '');
+
+      fireEvent(getByText('Forgot your password?'), 'press');
+
+      expect(spyAlert).toHaveBeenCalled();
+      expect(spyAlert.mock.calls[0][0]).toBe('Add Email');
+    });
+    it('should raise alert on faulty email when clicking forgot password', async function () {
+      spyAlert.mockClear();
+
+      const {getByText, getAllByPlaceholderText} = render(<LogInPage />);
+
+      const inputs = getAllByPlaceholderText('');
+      expect(inputs[0]).toBeTruthy();
+
+      fireEvent.changeText(inputs[0], 'emailwithoutaroba');
+
+      fireEvent(getByText('Forgot your password?'), 'press');
+
+      expect(spyAlert).toHaveBeenCalled();
+      expect(spyAlert.mock.calls[0][0]).toBe('Add Email');
+    });
+    it('should not raise alert when clicking forgot password with valid email', async function () {
+      spyAlert.mockClear();
+
+      const {getByText, getAllByPlaceholderText} = render(<LogInPage />);
+
+      const inputs = getAllByPlaceholderText('');
+      expect(inputs[0]).toBeTruthy();
+
+      fireEvent.changeText(inputs[0], '9jumpingpotatoes@gmail.com');
+
+      fireEvent(getByText('Forgot your password?'), 'press');
+
+      expect(spyAlert).toHaveBeenCalledTimes(0);
+    });
   });
 });
