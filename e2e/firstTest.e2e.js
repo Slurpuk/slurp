bb3086a832924d783b35e96b1f17ed7e8456668d
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-03-27 21:20:35 +0100

    Work towards setting up detox

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,3 +1,9 @@
+import firestore from '@react-native-firebase/firestore';
+import {fireEvent, render} from '@testing-library/react-native';
+import LogInPage from '../src/screens/LogInPage';
+import React from 'react';
+import auth from '@react-native-firebase/auth';
+
 describe('Example', () => {
   beforeAll(async () => {
     await device.launchApp();
@@ -7,7 +13,35 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
   //
-  it('should have welcome screen', async () => {
+  it('should be able to sign up', async () => {
+    async function registerUser() {
+      await auth()
+        .createUserWithEmailAndPassword('detoxuser@gmail.com', 'Password123')
+        .then(async () => {
+          await firestore().collection('Users').add({
+            Email: 'detoxuser@gmail.com',
+            FirstName: 'Detox',
+            LastName: 'Tester',
+          });
+        });
+    }
+    await registerUser();
+
+    const {getByText, getAllByPlaceholderText} = render(<LogInPage />);
+
+    const inputs = getAllByPlaceholderText('');
+    expect(inputs[0]).toBeTruthy();
+
+    const email = 'detoxuser@gmail.com';
+    const password = 'Password123';
+
+    fireEvent.changeText(inputs[0], email);
+    fireEvent.changeText(inputs[1], password);
+
+    expect(inputs[1]).toBeTruthy();
+
+    fireEvent(getByText('Log in'), 'press');
+
     await expect(element(by.id('emailfield'))).toBeVisible();
   });
   //
