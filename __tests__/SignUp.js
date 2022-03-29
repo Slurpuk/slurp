4630f74c6f3193ce8d90c4cae0f2349fd738455a
lisb0a <Saralatif06@gmail.com>
2022-03-29 18:03:29 +0100

    basic testing for sign up

--- __tests__/SignUp.js
+++ __tests__/SignUp.js
@@ -1,9 +1,8 @@
-import { fireEvent, render } from "@testing-library/react-native";
+import {fireEvent, render} from '@testing-library/react-native';
 import React from 'react';
 import {GlobalContext} from '../App';
 import SignUpPage from '../src/screens/SignUpPage';
-import { Alert } from "react-native";
-
+import {Alert} from 'react-native';
 
 describe('Login page', function () {
   const spyAlert = jest
@@ -13,13 +12,12 @@ describe('Login page', function () {
       callbackOrButtons[1].onPress(),
     );
 
-
-  describe('sing up', function() {
-    it('should sign up a user', function() {
+  describe('sing up', function () {
+    it('should sign up a user', function () {
       const globalContextMock = {
         isFirstTime: false,
       };
-      const { toJSON } = render(
+      const {toJSON} = render(
         <GlobalContext.Provider value={globalContextMock}>
           <SignUpPage />
         </GlobalContext.Provider>,
@@ -28,23 +26,40 @@ describe('Login page', function () {
       expect(toJSON()).toMatchSnapshot();
     });
 
-      it('should raise alert on submit with empty email', async function() {
-        const { getByText } = render(<SignUpPage />);
-
-        fireEvent(getByText('Sign up'), 'press');
-
-        expect(spyAlert).toHaveBeenCalled();
-        expect(spyAlert.mock.calls[0][0]).toBe('Empty Email');
-      });
-
-    it('should raise alert on submit with empty name', async function() {
-      const { getByText } = render(<SignUpPage />);
+    it('should raise alert on submit with empty first name', async function () {
+      const {getByText} = render(<SignUpPage />);
 
       fireEvent(getByText('Sign up'), 'press');
 
       expect(spyAlert).toHaveBeenCalled();
-      expect(spyAlert.mock.calls[0][0]).toBe('Empty Name');
-    });
+      expect(spyAlert.mock.calls[0][0]).toBe('Empty First Name');
     });
 
+    it('should raise alert on submit with empty last name', async function () {
+      const {getByText, getAllByPlaceholderText} = render(<SignUpPage />);
+
+      const inputs = getAllByPlaceholderText('');
+      expect(inputs[0]).toBeTruthy();
+      fireEvent.changeText(inputs[0], 'Jane');
+      fireEvent(getByText('Sign up'), 'press');
+
+      expect(spyAlert).toHaveBeenCalled();
+      expect(spyAlert.mock.calls[0][0]).toBe('Empty Last Name');
+    });
+
+    it('should raise alert on empty email', async function () {
+      const {getByText, getAllByPlaceholderText} = render(<SignUpPage />);
+
+      const inputs = getAllByPlaceholderText('');
+      expect(inputs[0]).toBeTruthy();
+
+      fireEvent.changeText(inputs[0], 'Jane');
+      fireEvent.changeText(inputs[1], 'Doe');
+
+      fireEvent(getByText('Sign up'), 'press');
+
+      expect(spyAlert).toHaveBeenCalled();
+      expect(spyAlert.mock.calls[0][0]).toBe('Empty Email');
+    });
+  });
 });
