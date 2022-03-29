fd57b041a649981a258a6edf8646bb0536f2d073
lisb0a <Saralatif06@gmail.com>
2022-03-29 15:48:27 +0100

    some more things

--- __tests__/Components.js
+++ __tests__/Components.js
@@ -5,20 +5,24 @@ import CustomButton from '../src/sub-components/CustomButton';
 import GreenHeader from '../src/sub-components/GreenHeader';
 import WhiteArrowButton from '../src/sub-components/WhiteArrowButton';
 import AnimatedCard from '../src/sub-components/AnimatedCard';
+import { fireEvent } from "@testing-library/react";
+import { Alert } from "react-native";
+
 
 describe('Custom sub-components', function () {
-  describe('button', function () {
-    it('should render correctly', function () {
+  describe('button', function() {
+    it('should render correctly', function() {
       const tree = renderer.create(<CustomButton />).toJSON();
       expect(tree).toMatchSnapshot();
     });
-    it('should render correctly with number indicator', function () {
+    it('should render correctly with number indicator', function() {
       const tree = renderer
         .create(<CustomButton optionalNumber={5} />)
         .toJSON();
       expect(tree).toMatchSnapshot();
     });
   });
+});
   describe('green header', function () {
     it('should render correctly', function () {
       const tree = renderer.create(<GreenHeader />).toJSON();
@@ -31,6 +35,10 @@ describe('Custom sub-components', function () {
       const tree = renderer.create(<WhiteArrowButton />).toJSON();
       expect(tree).toMatchSnapshot();
     });
+
+    it('should navigate to landing page', function() {
+      const tree = renderer.create(<WhiteArrowButton />).toJSON();
+    })
   });
 
   describe('animated card', function () {
@@ -39,4 +47,3 @@ describe('Custom sub-components', function () {
       expect(tree).toMatchSnapshot();
     });
   });
-});
--- __tests__/SignUp.js
+++ __tests__/SignUp.js
@@ -1,19 +1,50 @@
-import {render} from '@testing-library/react-native';
+import { fireEvent, render } from "@testing-library/react-native";
 import React from 'react';
 import {GlobalContext} from '../App';
 import SignUpPage from '../src/screens/SignUpPage';
+import { Alert } from "react-native";
 
-describe('sing up', function () {
-  it('should sign up a user', function () {
-    const globalContextMock = {
-      isFirstTime: false,
-    };
-    const {toJSON} = render(
-      <GlobalContext.Provider value={globalContextMock}>
-        <SignUpPage />
-      </GlobalContext.Provider>,
+
+describe('Login page', function () {
+  const spyAlert = jest
+    .spyOn(Alert, 'alert')
+    //@ts-ignore
+    .mockImplementation((title, message, callbackOrButtons) =>
+      callbackOrButtons[1].onPress(),
     );
-    expect(true).toBe(true);
-    expect(toJSON()).toMatchSnapshot();
-  });
+
+
+  describe('sing up', function() {
+    it('should sign up a user', function() {
+      const globalContextMock = {
+        isFirstTime: false,
+      };
+      const { toJSON } = render(
+        <GlobalContext.Provider value={globalContextMock}>
+          <SignUpPage />
+        </GlobalContext.Provider>,
+      );
+      expect(true).toBe(true);
+      expect(toJSON()).toMatchSnapshot();
+    });
+
+      it('should raise alert on submit with empty email', async function() {
+        const { getByText } = render(<SignUpPage />);
+
+        fireEvent(getByText('Sign up'), 'press');
+
+        expect(spyAlert).toHaveBeenCalled();
+        expect(spyAlert.mock.calls[0][0]).toBe('Empty Email');
+      });
+
+    it('should raise alert on submit with empty name', async function() {
+      const { getByText } = render(<SignUpPage />);
+
+      fireEvent(getByText('Sign up'), 'press');
+
+      expect(spyAlert).toHaveBeenCalled();
+      expect(spyAlert.mock.calls[0][0]).toBe('Empty Name');
+    });
+    });
+
 });
