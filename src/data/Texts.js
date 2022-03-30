ea53ae6009d45b7a78cad3a0c58b617a202b34c1
Jad Sbai <jad.sbai@kcl.ac.uk>
2022-03-30 19:55:23 +0100

    Improve empty lists texts

--- src/data/Texts.js
+++ src/data/Texts.js
@@ -1,9 +1,9 @@
 const emptyCurrentOrdersText =
-  'You currently have no orders running...\n\n Fancy a nice cup of coffee?';
+  'You have no orders in progress...\n\n Fancy a cup of coffee?';
 const emptyPastOrdersText =
-  "You don't have any history with us yet...\n\n Head over to the " +
+  "You don't have any past orders yet...\n\n Head over to the " +
   'home page to get started!';
 const emptyShopListText =
-  'It looks like there are no coffee shops close by...\n\n Try searching for one on the search bar, your journey starts there!';
+  'It looks like there are no coffee shops close by...\n\n Try searching for one on the search bar!';
 
 export {emptyShopListText, emptyCurrentOrdersText, emptyPastOrdersText};
