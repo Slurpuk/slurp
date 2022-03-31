a2bf5f93572b6eeefeb84bb1047c00b7d9aa85d0
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-03-31 19:05:38 +0100

    Initialise firebase in e2e test

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,50 +1,14 @@
-import React from 'react';
-// import {initializeApp} from 'firebase/app';
-// import {getAuth} from 'firebase/auth';
-// import {getFirestore} from 'firebase/firestore';
-// import {collection, addDoc} from 'firebase/firestore';
+import {initialiseFirebase, initialiseFirestore} from './firebaseSetUp';
+import {addDoc, collection, GeoPoint} from 'firebase/firestore';
 
-describe('Example', () => {
+let db;
+let auth;
+
+describe('Sign up', () => {
   beforeAll(async () => {
-    await device.launchApp();
-    // // Import the functions you need from the SDKs you need
-    //
-    // // https://firebase.google.com/docs/web/setup#available-libraries
-    //
-    // // Your web app's Firebase configuration
-    // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    // const firebaseConfig = {
-    //   apiKey: 'AIzaSyAr1toS2gSr-_6cMS4Jh0R2NhzI70g5nWk',
-    //   authDomain: 'independentcoffeeshops.firebaseapp.com',
-    //   databaseURL:
-    //     'https://independentcoffeeshops-default-rtdb.europe-west1.firebasedatabase.app',
-    //   projectId: 'independentcoffeeshops',
-    //   storageBucket: 'independentcoffeeshops.appspot.com',
-    //   messagingSenderId: '185382636935',
-    //   appId: '1:185382636935:web:e905902ac500f230f75722',
-    //   measurementId: 'G-5WK60CC02P',
-    // };
-    //
-    // const app = initializeApp(firebaseConfig);
-    // const auth = getAuth(app);
-    // const db = getFirestore(app);
-    //
-    // async function createUsers() {
-    //   const email = 'definitelynotafakeemail@gmail.com';
-    //   try {
-    //     const docRef = await addDoc(collection(db, 'users'), {
-    //       first: 'DetoxWithinTest',
-    //       last: 'Lovelace',
-    //       born: 1815,
-    //     });
-    //     console.log('Document written with ID: ', docRef.id);
-    //   } catch (e) {
-    //     console.error('Error adding document: ', e);
-    //   }
-    // }
-    //
-    // await createUsers();
-  });
+    const app = initialiseFirebase();
+    db = initialiseFirestore(app);
+    auth = initialiseFirebase(app);
 
   beforeEach(async () => {
     await device.reloadReactNative();
