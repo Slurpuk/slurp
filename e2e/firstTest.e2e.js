eed0eb40c50d87c91e86d75e8a58080b4defaa7c
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-04-01 15:31:00 +0100

    Document sign up tests

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -4,6 +4,21 @@ import {addDoc, collection, GeoPoint} from 'firebase/firestore';
 let db;
 let auth;
 
+/**
+ * This set of tests intends to test the transition from welcome pages to
+ * sign up and the possible backend errors one may encounter.
+ * The 'too many requests' error is untestable as:
+ * a) It would require making 101 sign up attempts.
+ * b) Firebase emulator doesn't actually consider this rule, this only applies
+ * to the cloud version.
+ * Network error is untestable on iOS as:
+ * a) The emulator works offline anyway so the only way to trigger a no network
+ * error would be to disconnect the emulator but this has to be done from the
+ * terminal.
+ * b)
+ * As in many other cases, there are untested catch or else alerts that should
+ * never be raised but exist a safety and therefore cannot be tested.
+ */
 describe('Sign up', () => {
   beforeAll(async () => {
     const app = initialiseFirebase();
@@ -15,11 +30,22 @@ describe('Sign up', () => {
 
   it('should be able to scroll through welcome pages and get to sign up page', async () => {
     await expect(element(by.id('welcome_pages'))).toBeVisible();
-    await expect(element(by.id('welcome_page1'))).toExist();
-    await element(by.id('welcome_pages_scrollview')).scrollTo('right');
-    await expect(element(by.id('welcome_page2'))).toExist();
-    //await element(by.id('welcome_pages_scrollview')).scrollTo('right');
-    await expect(element(by.id('welcome_page3'))).toExist();
+    await expect(element(by.id('welcome_page1'))).toBeVisible();
+    await expect(element(by.id('welcome_page2'))).not.toBeVisible();
+    await expect(element(by.id('welcome_page3'))).not.toBeVisible();
+
+    await element(by.id('welcome_pages_scrollview')).swipe('left');
+
+    await expect(element(by.id('welcome_page1'))).not.toBeVisible();
+    await expect(element(by.id('welcome_page2'))).toBeVisible();
+    await expect(element(by.id('welcome_page3'))).not.toBeVisible();
+
+    await element(by.id('welcome_pages_scrollview')).swipe('left');
+
+    await expect(element(by.id('welcome_page1'))).not.toBeVisible();
+    await expect(element(by.id('welcome_page2'))).not.toBeVisible();
+    await expect(element(by.id('welcome_page3'))).toBeVisible();
+
     await expect(element(by.text('Sign Up'))).toBeVisible();
     await element(by.text('Sign Up')).tap();
     await expect(element(by.id('welcome_pages'))).not.toBeVisible();
