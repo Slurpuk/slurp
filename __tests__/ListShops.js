b419c5f5a9bb45c7f922c0ab8e075af92a2806a4
lisb0a <Saralatif06@gmail.com>
2022-04-02 10:35:44 +0100

    popup

---
+++ __tests__/ListShops.js
@@ -0,0 +1,6 @@
+import {fireEvent, render} from '@testing-library/react-native';
+import React from 'react';
+import {GlobalContext} from '../App';
+import ShopList from "../src/components/Shops/ShopList";
+import {Alert} from 'react-native';
+
