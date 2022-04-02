3047cf719222c765a6fa5f22524e3bffeb5f6676
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2022-04-02 02:41:17 +0100

    Create file with log in tests

---
+++ e2e/logIn.e2e.js
@@ -0,0 +1,82 @@
+import {
+  initialiseAuth,
+  initialiseFirebase,
+  initialiseFirestore,
+} from './firebaseSetUp';
+import {addDoc, collection, GeoPoint} from 'firebase/firestore';
+import {createUserWithEmailAndPassword} from 'firebase/auth';
+
+let db;
+let auth;
+
+describe('Log in', () => {
+  beforeAll(async () => {
+    const app = initialiseFirebase();
+    db = initialiseFirestore(app);
+    auth = initialiseAuth(app);
+
+    await device.launchApp(); // deletes async storage on launch ensuring welcome pages are displayed
+  });
+  it('should raise alert if forgot password with existing email', async () => {
+    const usedFirstName = 'Boris';
+    const usedLastName = 'Johnson';
+    const usedEmail = 'getbrexitdone@example.org';
+    const password = 'Password123!';
+    await addDoc(collection(db, 'users'), {
+      first_name: usedFirstName,
+      last_name: usedLastName,
+      email: usedEmail,
+      location: new GeoPoint(51.503223, -0.1275),
+    });
+    await createUserWithEmailAndPassword(auth, usedEmail, password);
+    await expect(element(by.id('log_in_page'))).toBeVisible();
+    await element(by.id('log_in_page_email')).typeText(usedEmail);
+    await expect(element(by.text('Forgot your password?'))).toBeVisible();
+    await element(by.text('Forgot your password?')).tap();
+    await expect(
+      element(by.type('_UIAlertControllerActionView')),
+    ).toBeVisible(); // Check raises alert
+    await element(by.type('_UIAlertControllerActionView')).tap();
+    await expect(element(by.id('log_in_page'))).toBeVisible();
+  });
+  it('should also raise alert if forgot password with non-existent email', async () => {
+    const email = 'thisemaildoesnotexist@example.org';
+    await expect(element(by.id('log_in_page'))).toBeVisible();
+    await element(by.id('log_in_page_email')).typeText(email);
+    await expect(element(by.text('Forgot your password?'))).toBeVisible();
+    await element(by.text('Forgot your password?')).tap();
+    await expect(
+      element(by.type('_UIAlertControllerActionView')),
+    ).toBeVisible(); // Check raises alert
+    await element(by.type('_UIAlertControllerActionView')).tap();
+    await expect(element(by.id('log_in_page'))).toBeVisible();
+  });
+  it('should raise alert if log in wrong password', async () => {
+    const usedFirstName = 'Boris';
+    const usedLastName = 'Johnson';
+    const usedEmail = 'getbrexitdone@example.org';
+    const password = 'Password123!';
+    const wrongPassword = 'WrongPassword';
+    await addDoc(collection(db, 'users'), {
+      first_name: usedFirstName,
+      last_name: usedLastName,
+      email: usedEmail,
+      location: new GeoPoint(51.503223, -0.1275),
+    });
+    await createUserWithEmailAndPassword(auth, usedEmail, password);
+    await element(by.id('sign_up_page_email')).typeText(usedEmail);
+    await element(by.id('sign_up_page_password')).typeText(password);
+    await element(by.text('Create Account')).tap();
+    await expect(
+      element(by.type('_UIAlertControllerActionView')),
+    ).toBeVisible(); // Check raises alert
+    await element(by.type('_UIAlertControllerActionView')).tap();
+    await expect(element(by.id('sign_up_page'))).toBeVisible();
+    auth.currentUser.delete;
+    await element(by.id('sign_up_page_first_name')).replaceText('');
+    await element(by.id('sign_up_page_last_name')).replaceText('');
+    await element(by.id('sign_up_page_email')).replaceText('');
+    await element(by.id('sign_up_page_password')).replaceText('');
+    await element(by.id('sign_up_page_confirm_password')).replaceText('');
+  });
+});
