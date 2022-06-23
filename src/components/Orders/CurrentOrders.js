76fbd2403ea48169b2b18b8a6df5bdd211a6cdd8
Jad Sbai <jad.sbai@kcl.ac.uk>
2022-06-23 17:23:49 +0100

    Adapt the Basket page to the new Reducer logic

--- src/components/Orders/CurrentOrders.js
+++ src/components/Orders/CurrentOrders.js
@@ -1,19 +1,21 @@
 import {FlatList} from 'react-native';
 import CollapsableOrder from './CollapsableOrder';
 import EmptyListText from '../../sub-components/EmptyListText';
-import React, { useContext } from "react";
-import { calculateDistance } from "../../helpers/screenHelpers";
-import { GlobalContext } from "../../../App";
+import React, {useContext} from 'react';
+import {calculateDistance} from '../../helpers/screenHelpers';
+import {GlobalContext} from '../../contexts';
 
 export const CurrentOrders = props => {
-  const context = useContext(GlobalContext);
+  const {globalState} = useContext(GlobalContext);
   props.currentOrders.forEach(order => {
     order.ETA = calculateDistance(order.shop.location, {
-      latitude: context.currentUser.location._latitude,
-      longitude: context.currentUser.location._longitude,
+      latitude: globalState.currentUser.location._latitude,
+      longitude: globalState.currentUser.location._longitude,
     });
   });
-  props.currentOrders.sort((a, b) => (a.ETA < b.ETA ? - 1 : Number(a.ETA > b.ETA)))
+  props.currentOrders.sort((a, b) =>
+    a.ETA < b.ETA ? -1 : Number(a.ETA > b.ETA),
+  );
   return (
     <>
       <FlatList
